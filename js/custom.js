8bc56da38774a88fcee6ebf9ebf561b1492b8899
mojahid213 <mojoffi133@gmail.com>
2021-01-26 16:06:39 +0600

    demo bank

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+const submitBtn = document.getElementById("loginBtn");
+submitBtn.addEventListener("click", function(){
+   const loginPage = document.getElementById("login-area");
+   loginPage.style.display="none";
+})
